8ff9e145420e4d4b37623c7925f587aa91a07e77
VBR1997 <vijaybhaskarreddy1997@gmail.com>
2023-08-29 10:50:41 +0530

    first commit

---
+++ src/main/java/com/cyr/restfulservice/RestfulwebserviceProjrctApplication.java
@@ -0,0 +1,14 @@
+package com.cyr.restfulservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulwebserviceProjrctApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulwebserviceProjrctApplication.class, args);
+		System.out.println("vijaybhaskar....");
+	}
+
+}
